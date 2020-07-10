17b233e7242baf577d20b538bf2efd614c668ac5
h-dj <1432517356@qq.com>
2020-07-10 17:19:48 +0800

    1. consul作为服务注册与发现

---
+++ consul-waiter-service/src/main/java/cn/hdj/waiterService/WaiterController.java
@@ -0,0 +1,24 @@
+package cn.hdj.waiterService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author hdj
+ * @version 1.0
+ * @date 7/3/20 3:10 PM
+ * @description:
+ */
+
+@RestController
+public class WaiterController {
+
+    @GetMapping(value = "/coffee", produces = "application/json")
+    public List<String> coffeeMenu(HttpServletRequest request) {
+        return Arrays.asList(request.getServerPort()+" ", "ESPRESSO COFFEES $12", "HANDMADE COFFEES $15", "SOOTHING HOT ALTERNATIVES $20", "COLD ALTERNATIVES $8");
+    }
+}
