3487098abfc26789776b2c1eff6edadaf8e2b9c1
h-dj <1432517356@qq.com>
2020-07-11 15:11:16 +0800

    1. nacos作为服务注册与发现

---
+++ nacos-waiter-service/src/test/java/cn/hdj/eurakaWaiterService/EurakaWaiterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hdj.eurakaWaiterService;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaWaiterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
